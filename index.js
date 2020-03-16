ea630eeb3eeb2db88ed9d93348b07a32ef9492ac
Sean Dutton <iceman198@gmail.com>
2020-03-16 12:31:59 -0400

    m

--- index.js
+++ index.js
@@ -1,17 +1,66 @@
 "use strict";
-
+let exec = require('child_process').exec;
 let display = require('./modules/display_old.js');
+const express = require('express');
+//const url = require('url');
+const app = express();
+const port = 80;
 
 let myIp = "";
 
 startup();
 console.log('Ready');
 
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/" + "index.html");
+});
+
+app.get('/service/', (request, response) => {
+    //var url_parts = url.parse(request.url, true);
+    //var query = url_parts.query;
+    let cmd = request.query.cmd;
+    console.log(`cmd is ${cmd}`);
+    if (cmd) {
+        if (cmd == "shutdown") {
+            console.log('sending command: ' + cmd);
+            response.sendStatus(`Shutdown initiated`);
+            shutdown();
+        } else {
+            response.sendStatus(`Command not recognized`);
+        }
+
+    }
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+
+    console.log(`server is listening on ${port}`)
+});
+
+function shutdown() {
+    let lines = [{ "text": "Shutting down" }];
+    display.write(1, lines);
+    let dir = exec(`shutdown now`, function (err, stdout, stderr) {
+        if (err) {
+            console.log('error sending command: ', err);
+        }
+        console.log(stdout);
+    });
+
+    dir.on('exit', function (code) {
+        console.log('exit complete with code ', code);
+    });
+}
+
 function startup() {
     getIP();
     let mytext = 'this is a test';
-    console.log(`I'm sending: ${mytext}`);
-    display.write([mytext, 'two', '', 'four']);
+    console.log(`IP: ${myIp}`);
+    display.write([mytext, 'two', 'three', 'four']);
 }
 
 function getIP() {
