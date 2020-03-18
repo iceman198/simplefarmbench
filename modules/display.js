48e688974dc3fed589cbf453a4a6db72dae9edd4
Sean Dutton <iceman198@gmail.com>
2020-03-18 10:19:04 -0400

    m

--- modules/display.js
+++ modules/display.js
@@ -11,7 +11,7 @@ let oled_opts = {
     width: 128, // screen width
     height: 32, // screen height
     address: 0x3C, // Pass I2C address of screen if it is not the default of 0x3C
-//    device: '/dev/i2c-1', // Pass your i2c device here if it is not /dev/i2c-1
+    device: '/dev/i2c-1', // Pass your i2c device here if it is not /dev/i2c-1
 //    microview: true, // set to true if you have a microview display
 };
 
