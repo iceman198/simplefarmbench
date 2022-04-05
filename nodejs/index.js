af64414b1566604f8fbdfa98c10ecb7b5c2a5518
Sean Dutton <iceman198@gmail.com>
2022-04-05 10:25:28 -0400

    m

--- nodejs/index.js
+++ nodejs/index.js
@@ -132,9 +132,9 @@ function startup() {
         Logger.log('info', 'index.js', `${tempF}","${tempC}","${lampStatus}","${humidity}","${isDaytime()}`);
         oled.setCursor(1, 1);
         oled.writeString(font, 1, `Temp: ${tempF}`, 1, true);
-        oled.setCursor(1, 2);
+        oled.setCursor(2, 1);
         oled.writeString(font, 1, `Hum: ${humidity}`, 1, true);
-        oled.setCursor(1,3);
+        oled.setCursor(3,1);
         oled.writeString(font, 1, `Lamp: ${lampStatus}`, 1, true);
 
         //console.log(`About to write to the display`);
